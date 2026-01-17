38f1f383590cc74e96824bea545bb139102a83f9
asdasdsdsadsada <sarkhas@mail.ru>
2026-01-17 23:47:58 +0300

    only bittorrent

--- ipt/libxt_ndpi.c
+++ ipt/libxt_ndpi.c
@@ -3,68 +3,64 @@
 #include <xtables.h>
 #include "xt_ndpi.h"
 
-#define NDPI_OPT_OFFSET 100
+/* Определяем ID для битторрента. 
+   В nDPI протокол BitTorrent обычно имеет индекс 2, 
+   но мы можем назначить любой ID для парсера, главное потом правильно сопоставить */
+enum {
+    O_BITTORRENT = 1,
+};
 
-static char *prot_long_str[] = { NDPI_PROTOCOL_LONG_STRING };
-static char *prot_short_str[] = { NDPI_PROTOCOL_SHORT_STRING };
-
-/* Используем современную структуру для nf_tables */
-static struct xt_option_entry ndpi_mt_opts_x6[NDPI_LAST_NFPROTO + 1];
+/* Современная структура опций X6 */
+static const struct xt_option_entry ndpi_mt_opts_x6[] = {
+    {.name = "bittorrent", .id = O_BITTORRENT, .type = XTTYPE_NONE},
+    {.name = NULL} // Терминатор
+};
 
 static void ndpi_mt_help(void) {
-    int i;
-    printf("ndpi match options:\n");
-    for (i = 1; i < NDPI_LAST_NFPROTO; i++) {
-        if (prot_short_str[i] != NULL)
-            printf("--%-20s Match for %s\n", prot_short_str[i], prot_long_str[i]);
-    }
-}
-
-/* Функция проверки пустоты маски (безопасная реализация) */
-static int is_mask_empty(const NDPI_PROTOCOL_BITMASK *mask) {
-    int i;
-    for (i = 0; i < NDPI_NUM_FDS_BITS; i++) {
-        if (mask->fds_bits[i] != 0) return 0;
-    }
-    return 1;
+    printf("ndpi match options:\n"
+           "--bittorrent    Match for BitTorrent protocol\n");
 }
 
 /* Современный парсер X6 */
 static void ndpi_mt_x6_parse(struct xt_option_call *cb) {
     struct xt_ndpi_mtinfo *info = cb->data;
-    // Вычитаем смещение, чтобы вернуться к индексу nDPI (1, 2, 3...)
-    unsigned int proto_id = cb->entry->id - NDPI_OPT_OFFSET;
 
-    if (proto_id > 0 && proto_id < NDPI_LAST_NFPROTO) {
-        NDPI_ADD_PROTOCOL_TO_BITMASK(info->flags, proto_id);
+    switch (cb->entry->id) {
+        case O_BITTORRENT:
+            /* Здесь мы используем константу NDPI_PROTOCOL_BITTORRENT 
+               из заголовочных файлов nDPI */
+            NDPI_ADD_PROTOCOL_TO_BITMASK(info->flags, NDPI_PROTOCOL_BITTORRENT);
+            break;
     }
 }
 
-/* Проверка параметров перед отправкой в ядро */
+/* Проверка, что хоть что-то выбрано */
 static void ndpi_mt_x6_check(struct xt_fcheck_call *cb) {
     struct xt_ndpi_mtinfo *info = cb->data;
-    if (is_mask_empty(&info->flags)) {
-        xtables_error(PARAMETER_PROBLEM, "xt_ndpi: You must specify at least one protocol");
+    int i, found = 0;
+
+    for (i = 0; i < NDPI_NUM_FDS_BITS; i++) {
+        if (info->flags.fds_bits[i] != 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        xtables_error(PARAMETER_PROBLEM, "xt_ndpi: You must specify --bittorrent");
     }
 }
 
 static void ndpi_mt_print(const void *ip, const struct xt_entry_match *match, int numeric) {
     const struct xt_ndpi_mtinfo *info = (const void *)match->data;
-    int i;
-    printf(" ndpi ");
-    for (i = 1; i < NDPI_LAST_NFPROTO; i++) {
-        if (NDPI_COMPARE_PROTOCOL_TO_BITMASK(info->flags, i)) 
-            printf("%s ", prot_short_str[i]);
-    }
+    if (NDPI_COMPARE_PROTOCOL_TO_BITMASK(info->flags, NDPI_PROTOCOL_BITTORRENT))
+        printf(" ndpi bittorrent");
 }
 
 static void ndpi_mt_save(const void *ip, const struct xt_entry_match *match) {
     const struct xt_ndpi_mtinfo *info = (const void *)match->data;
-    int i;
-    for (i = 1; i < NDPI_LAST_NFPROTO; i++) {
-        if (NDPI_COMPARE_PROTOCOL_TO_BITMASK(info->flags, i)) 
-            printf(" --%s", prot_short_str[i]);
-    }
+    if (NDPI_COMPARE_PROTOCOL_TO_BITMASK(info->flags, NDPI_PROTOCOL_BITTORRENT))
+        printf(" --bittorrent");
 }
 
 static struct xtables_match ndpi_mt_reg = {
@@ -83,18 +79,5 @@ static struct xtables_match ndpi_mt_reg = {
 };
 
 void __attribute__((constructor)) libxt_ndpi_setup(void) {
-    int i;
-    int opt_idx = 0;
-    memset(ndpi_mt_opts_x6, 0, sizeof(ndpi_mt_opts_x6));
-
-    for (i = 1; i < NDPI_LAST_NFPROTO; i++) {
-        if (prot_short_str[i] != NULL && strlen(prot_short_str[i]) > 0) {
-            ndpi_mt_opts_x6[opt_idx].name = prot_short_str[i];
-            ndpi_mt_opts_x6[opt_idx].type = XTTYPE_NONE;
-            // Добавляем смещение, чтобы избежать конфликта с системными ID (0-64)
-            ndpi_mt_opts_x6[opt_idx].id   = i + NDPI_OPT_OFFSET; 
-            opt_idx++;
-        }
-    }
     xtables_register_match(&ndpi_mt_reg);
 }
